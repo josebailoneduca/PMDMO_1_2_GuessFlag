c46e849099da4070d79a314dd5045d362290d193
Jose Javier Bailón Ortiz <josebailon@gmail.com>
2023-09-30 04:42:46 +0200

    Version 1

---
+++ app/src/main/java/com/example/guessflag/Aplicacion.java
@@ -0,0 +1,211 @@
+package com.example.guessflag;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.res.TypedArray;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Clase principal de la aplicacion. Contiene la logica de funcionamiento de una partida y el estado
+ * general de partida iniciada o no.
+ */
+public class Aplicacion extends Application {
+
+
+
+
+    /**
+     * Array de id/resource banderas
+     */
+    TypedArray banderas;
+    /**
+     * Array de nombres de paises
+     */
+    String[] paises;
+
+    /**
+     * Determina si la partida esta empezada o no
+     */
+    boolean partidaEmpezada;
+
+    /**
+     * Almacena el pais correcto para la pregunta actual
+     */
+    int correcto;
+
+    /**
+     * Almacena la id de los paises de la pregunta actual
+     * indice 0 bandera de btn1, indice 1   bandera de btn2, indice  bandera de 2 btn3
+     */
+    ArrayList<Integer> idPaisesEnBotones;
+
+    /**
+     * Almacena los paises acertados
+     */
+    ArrayList<Integer> acertados;
+    /**
+     * Almacena el nivel maximo que se ha alcanzado
+     */
+    private int record;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //inicializar los atributos
+        iniciarAtributos();
+    }
+
+    /**
+     * Inicializa los valores de los atributos
+     */
+    private void iniciarAtributos() {
+        //Recoger banderas y paises desde los recursos
+        //Los paises y sus banderas se almacenan en los recursos en res/values/paises.xml
+        //donde hay especificados dos arrays: uno con el nombre y otro con la referencia a la imagen de su bandera.
+        //El identificador de los paises en la logica de la partida es el indice del pais dentro
+        //de estos arrays
+        this.banderas = getResources().obtainTypedArray(R.array.banderas);
+        this.paises = getResources().getStringArray(R.array.paises);
+
+        //inicializa el almacen de las id de paises para usar en los botones
+        this.idPaisesEnBotones = new ArrayList<Integer>();
+
+        //inicializa el almacen de las id de paises acertados
+        this.acertados = new ArrayList<Integer>();
+
+        //inicializacion del estado de partida
+        this.partidaEmpezada=false;
+
+        //inicializa el pais correcto actual
+        this.correcto = -1;
+
+        //inicializa el nivel maximo alcanzado
+        this.record =0;
+    }
+
+
+    /**
+     * Gestiona el inicio de una partida
+     */
+    public void iniciarPartida() {
+        //borrar los listados de acertados y paises de la pregunta actual
+        this.acertados.clear();
+        this.idPaisesEnBotones.clear();
+        //elije los paises para la pregunta actual
+        cambiarPais();
+        //marca la partida como empezada
+        this.partidaEmpezada=true;
+    }
+
+
+    /**
+     * Define el nuevo pais a acertar y los paises cuyas banderas componen la proxima pregunta
+     */
+    @SuppressLint("ResourceType")
+    private void cambiarPais() {
+        //recoger pais a acertar(-1,-1 para incluir a todos los que no han sido ya acertados)
+        this.correcto=nuevoPais(-1,-1);
+
+        //recoger primer pais incorrecto(evita tambien el correcto seleccionado)
+        int incorrecto1=nuevoPais(this.correcto,-1);
+
+        //recoger el segundo pais incorrecto (evita el correcto seleccionado y el incorrecto1)
+        int incorrecto2=nuevoPais(this.correcto,incorrecto1);
+
+        //Mezcla al azar las posiciones de los paises
+        this.idPaisesEnBotones = new ArrayList<Integer>();
+        this.idPaisesEnBotones.add(this.correcto);
+        this.idPaisesEnBotones.add(incorrecto1);
+        this.idPaisesEnBotones.add(incorrecto2);
+        Collections.shuffle(this.idPaisesEnBotones,new Random());
+    }
+
+
+    /**
+     * Devuelve un pais elegido al azar que no aparezca entre los acertados y que no sea ninguno de
+     * los paises pasados en los  parametros de invalidos para la seleccion
+     * @param invalido1 Id del pais 1 invalido para la seleccion
+     * @param invalido2 Id del pais 2 invalido para la seleccion
+     * @return el pais seleccionado
+     */
+    private int nuevoPais(int invalido1,int invalido2) {
+        Random r = new Random();
+        boolean encontrado=false;
+        int candidato=-1;
+        while(!encontrado){
+            candidato=r.nextInt(this.paises.length);
+            if (!this.acertados.contains(candidato)&&candidato!=invalido1 && candidato!=invalido2){
+                encontrado=true;
+            }
+        }
+        return candidato;
+    }
+
+
+    /**
+     * Gestiona una contestacion a la pregunta actual
+     * @param i Indice de la respuesta seleccionada segun lo almacenado en el array: this.idPaisesEnBotones
+     * @return true si es respuesta correcta y false si es incorrecta
+     */
+    public boolean contestacion(int i){
+        if (this.idPaisesEnBotones.get(i)==this.correcto)  {
+            //agrega el pais acertado al listado de acertados
+            this.acertados.add(this.correcto);
+            //genera los datos para la siguiente pregunta
+            cambiarPais();
+            return true;
+        }
+        else {
+            //actualizar el nivel maximo si corresponde
+            if (this.record <this.acertados.size())
+                this.record =this.acertados.size();
+            //da la partida por finalizada
+            this.partidaEmpezada=false;
+            return false;
+        }
+    }
+
+
+    /**
+     * Devuelve si la partida esta empezada
+     * @return True si esta empezada. False si no esta empezada
+     */
+    public boolean isPartidaEmpezada() {
+        return partidaEmpezada;
+    }
+
+    /**
+     * Devuelve el identificador del recurso de imagen de la bandera del pais con la id suministrada
+     * @param i Id del pais
+     * @return ID de recurso de imagen de la bandera del pais
+     */
+    public int getBandera(int i) {
+        return banderas.getResourceId(this.idPaisesEnBotones.get(i),-1);
+    }
+
+    /**
+     * Devuelve el nombre del pais correcto para la pregunta actual
+     * @return El nombre del pais
+     */
+    public String getNombrePaisCorrecto() {
+        return this.paises[this.correcto];
+    }
+
+    /**
+     * Devuelve el nivel de la partida en el que se esta. Se empieza en nivel 0 y se incrementara
+     * en 1 por cada bandera acertada
+     *
+     * @return El nivel actual
+     */
+    public int getNivel() {
+        return this.acertados.size();
+    }
+
+    public int getRecord(){
+        return this.record;
+    }
+}
---
+++ app/src/main/java/com/example/guessflag/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.guessflag;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Clase de vista principal. Gestiona los eventos de pulsado de los botones y la configuracion
+ * del layout dependiendo del estado de la aplicacion.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /**
+     * Referencia a la aplicacion
+     */
+    Aplicacion aplicacion;
+
+    /**
+     * layout con los elementos del menu inicial
+     */
+    ConstraintLayout layoutMenu;
+
+    /**
+     * layout con los elementos de partida
+     */
+    ConstraintLayout layoutPartida;
+
+
+    /**
+     * Boton iniciar partida
+     */
+    Button btnInicarPartida;
+
+
+    /**
+     * TextView que muestra el pais a acertar
+     */
+    private TextView tvPais;
+    /**
+     * TextView que muestra el nivel actual
+     */
+    private TextView tvNivel;
+    /**
+     * TextView que muestra el record maximo alcanzado
+     */
+    private TextView tvRecord;
+
+    /**
+     * Boton de bandera superior/izquierdo
+     */
+    ImageButton btn1;
+
+    /**
+     * Boton bandera central
+     */
+    ImageButton btn2;
+
+    /**
+     * Boton bandera inferior/derecho
+     */
+    ImageButton btn3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //recoger referencia a la aplicacion
+        this.aplicacion = (Aplicacion) getApplication();
+        //define el layout a mostrar
+        setContentView(R.layout.activity_main);
+        //inicializa los atributos de la activity
+        iniciarAtributos();
+        //inicializa los eventos de la activity
+        inicializarEventos();
+        //actualiza el estado del layout
+        actualizarEstado();
+    }
+
+
+    /**
+     * Inicializa los atributos de la clase
+     */
+    private void iniciarAtributos() {
+        //recoger referencias a elementos de interface
+        this.layoutMenu = findViewById(R.id.layoutMenu);
+        this.layoutPartida = findViewById(R.id.layoutPartida);
+        this.btnInicarPartida = findViewById(R.id.btnIniciaPartida);
+        this.btn1 = findViewById(R.id.btn1);
+        this.btn2 = findViewById(R.id.btn2);
+        this.btn3 = findViewById(R.id.btn3);
+        this.tvPais = findViewById(R.id.tvPais);
+        this.tvNivel = findViewById(R.id.tvNivel);
+        this.tvRecord = findViewById(R.id.tvRecord);
+    }
+
+
+    /**
+     * Inicializa los eventos de la activity
+     */
+    private void inicializarEventos() {
+        //onclick de botones
+        this.btnInicarPartida.setOnClickListener(this);
+        this.btn1.setOnClickListener(this);
+        this.btn2.setOnClickListener(this);
+        this.btn3.setOnClickListener(this);
+    }
+
+    /**
+     * Actualiza el estado del layout en funcion de si la partida esta empezada o no
+     */
+    private void actualizarEstado() {
+        if (this.aplicacion.isPartidaEmpezada()) {
+            mostrarLayoutPartida();
+        } else {
+            mostrarLayoutMenu();
+        }
+
+    }
+
+    /**
+     * Configura el layout en modo partida
+     */
+    private void mostrarLayoutPartida() {
+        //activar layout de partida y ocultar el layout de menu
+        this.layoutPartida.setVisibility(View.VISIBLE);
+        this.layoutMenu.setVisibility(View.GONE);
+        //actualizar imagenes de botones
+        this.btn1.setImageResource(this.aplicacion.getBandera(0));
+        this.btn2.setImageResource(this.aplicacion.getBandera(1));
+        this.btn3.setImageResource(this.aplicacion.getBandera(2));
+        //actualizar etiqueta de pais a acertar
+        this.tvPais.setText(this.aplicacion.getNombrePaisCorrecto());
+        //actualizar etiquetas de nivel y record
+        this.tvNivel.setText("Nivel: " + this.aplicacion.getNivel());
+        this.tvRecord.setText("Record: " + this.aplicacion.getRecord());
+
+    }
+
+    /**
+     * Configura el layout en modo menu
+     */
+    private void mostrarLayoutMenu() {
+        //activar layout de menu y ocultar el de partida
+        this.layoutPartida.setVisibility(View.GONE);
+        this.layoutMenu.setVisibility(View.VISIBLE);
+    }
+
+
+    /**
+     * Gestiona el pulsado de botones
+     * @param view Vista("boton") que ha generado el evento
+     */
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnIniciaPartida) {
+            this.aplicacion.iniciarPartida();
+            this.actualizarEstado();
+        } else if (view.getId() == R.id.btn1)
+            gestionarContestacion(0);
+        else if (view.getId() == R.id.btn2)
+            gestionarContestacion(1);
+        else if (view.getId() == R.id.btn3)
+            gestionarContestacion(2);
+    }
+
+    /**
+     * Gestiona la contestacion a la pregunta
+     * @param i indice de la respuesta elegida
+     */
+    private void gestionarContestacion(int i) {
+        //pasar a la aplicacion la contestacion y recoger el resultado
+        boolean acierto = this.aplicacion.contestacion(i);
+        //Muestra el mensaje segun se acierte o no y actualiza el estado del layout
+        if (acierto) {
+            Toast.makeText(this, "¡Acertaste!", Toast.LENGTH_SHORT).show();
+            actualizarEstado();
+        } else {
+            toast("¡GAME OVER! Acertaste "+this.aplicacion.getNivel()+" banderas. El record está en "+this.aplicacion.getRecord()+" ¡A estudiar toca!");
+            actualizarEstado();
+        }
+    }
+
+    /**
+     * Muestra un toast con el texto pasado
+     * @param texto Texto a mostrar en el toast
+     */
+    private void toast(String texto) {
+        Toast t = Toast.makeText(this, texto, Toast.LENGTH_LONG);
+        t.setGravity(Gravity.FILL_HORIZONTAL, 0, 0);
+        t.show();
+    }
+}
+
